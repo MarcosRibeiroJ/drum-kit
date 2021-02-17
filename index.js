8743a6d93481a52d6c5dc359757c443ae18f976c
Marcos Ribeiro <marcossrj90@gmail.com>
2021-02-17 20:33:01 -0300

    adding animation to the buttons

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         var buttonInnerHTML = this.innerHTML;
         
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keydown", function(event) {
+    
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -52,4 +57,13 @@ function makeSound(key) {
             break;
     }
 
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
